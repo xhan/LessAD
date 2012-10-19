5f439dc0ca4f9111f9626d7f482df08425cca9eb
xu han <xhan87@gmail.com>
2012-10-19 22:42:17 +0800

    shared headers

---
+++ shared/LessAD.h
@@ -0,0 +1,9 @@
+#define PLIST_FILE @"/var/mobile/Library/Preferences/com.xhan.LessAD.plist"
+#define KEYON @"ABEnabled"
+#define KEYBLOCKCNT @"BlockCnt"
+#define VERSION @"0.7.0"
+
+
+#define KEY_FILTER_COMMON @"FILTER_COMMON"
+#define KEY_FILTER_WEIBO  @"FILTER_WEIBO"
+#define KEY_FILTER_QQ     @"FILTER_QQ"
